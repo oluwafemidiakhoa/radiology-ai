ad4b31e97822267bac7621e90eb5b8fdb663dedb
Oluwafemi Idiakhoa <oluwafemidiakhoa@gmail.com>
2025-02-17 09:21:52 -0600

    Removed secrets and updated .gitignore

--- backend/config.py
+++ backend/config.py
@@ -1,11 +1,9 @@
 import os
 from dotenv import load_dotenv
 
-# Load environment variables from .env file
 load_dotenv()
 
-# Get OpenAI API Key
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 if not OPENAI_API_KEY:
-    raise ValueError("Missing OpenAI API Key. Set it in the .env file.")
+    raise ValueError("Missing OpenAI API Key! Set it in environment variables.")
